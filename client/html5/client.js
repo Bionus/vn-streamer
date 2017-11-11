d6b20b8bd28df460dad62db1aa25214fe8ae4dcb
Bionus <bio.nus@hotmail.fr>
2017-11-11 19:11:42 +0100

    Add server IP field to HTML5 client

--- client/html5/client.js
+++ client/html5/client.js
@@ -1,7 +1,45 @@
-window.onload = function() {
+function stringToColour(str) {
+	var hash = 0;
+	for (var i = 0; i < str.length; i++) {
+		hash = str.charCodeAt(i) + ((hash << 5) - hash);
+	}
+	var colour = '#';
+	for (var i = 0; i < 3; i++) {
+		var value = (hash >> (i * 8)) & 0xFF;
+		colour += ('00' + value.toString(16)).substr(-2);
+	}
+	return colour;
+}
+
+var nextBlob = '';
+function commandReceived(command) {
+	nextBlob = '';
+	var arr = command.split('|');
+	if (arr[0] == 'text') {
+		textReceived(arr[1], arr[2]);
+	} else if (arr[0] == 'image') {
+		nextBlob = 'image';
+	}
+}
+function textReceived(name, text) {
+	var character = document.getElementById('character');
+	character.innerHTML = name;
+	if (name.length > 0) {
+		character.style.color = stringToColour(name);
+	}
+	document.getElementById('text').innerHTML = text;
+}
+function imageReceived(image) {
+	if (nextBlob == 'image' || nextBlob == '') {
+		document.getElementById('image').src = image;
+		nextBlob = '';
+	}
+}
+
+function connect(ip) {
 	var status = document.getElementById('connection-status-text');
 	status.innerHTML = 'Pending';
-	var connection = new WebSocket('ws://localhost:46421');
+	var connection = new WebSocket('ws://' + ip);
 	connection.onopen = function() {
 		status.innerHTML = 'Connected';
 		connection.send('0 0 0');
@@ -13,45 +51,6 @@ window.onload = function() {
 		status.innerHTML = 'Error';
 		console.log('WebSocket error', event);
 	};
-
-	function stringToColour(str) {
-		var hash = 0;
-		for (var i = 0; i < str.length; i++) {
-			hash = str.charCodeAt(i) + ((hash << 5) - hash);
-		}
-		var colour = '#';
-		for (var i = 0; i < 3; i++) {
-			var value = (hash >> (i * 8)) & 0xFF;
-			colour += ('00' + value.toString(16)).substr(-2);
-		}
-		return colour;
-	}
-
-	var nextBlob = '';
-	function commandReceived(command) {
-		nextBlob = '';
-		var arr = command.split('|');
-		if (arr[0] == 'text') {
-			textReceived(arr[1], arr[2]);
-		} else if (arr[0] == 'image') {
-			nextBlob = 'image';
-		}
-	}
-	function textReceived(name, text) {
-		var character = document.getElementById('character');
-		character.innerHTML = name;
-		if (name.length > 0) {
-			character.style.color = stringToColour(name);
-		}
-		document.getElementById('text').innerHTML = text;
-	}
-	function imageReceived(image) {
-		if (nextBlob == 'image' || nextBlob == '') {
-			document.getElementById('image').src = image;
-			nextBlob = '';
-		}
-	}
-
 	connection.onmessage = function(event) {
 		if (event.data instanceof Blob) {
 			reader = new FileReader();
@@ -70,5 +69,15 @@ window.onload = function() {
 
 	document.getElementById('control-next').onclick = function() {
 		connection.send('1');
+	};
+};
+
+window.onload = function() {
+	var status = document.getElementById('connection-status-text');
+	status.innerHTML = 'Disconnected';
+
+	document.getElementById('connect-form').onsubmit = function() {
+		connect(document.getElementById('ip').value);
+		return false;
 	};
 };
